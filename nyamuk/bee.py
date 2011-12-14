4fdf71eecaf68d6504817537a7775a1d42783e01
Iwan BK <iwan.b.kusnanto@gmail.com>
2011-12-15 05:40:39 +0700

    Server sudah bisa menerima SUBSCRIBE dan membalas dengan SUBACK. Subscription belum dimasukkan dengan benar ke Subscription Manager

--- nyamuk/bee.py
+++ nyamuk/bee.py
@@ -8,7 +8,7 @@ from MV import MV
 from mqtt_pkt import MqttPkt
 
 class Bee(nyamuk.Nyamuk):
-    def __init__(self, sock, addr):
+    def __init__(self, sock, addr, conn_mgr, subs_mgr):
         nyamuk.Nyamuk.__init__(self)
         
         #from nyamuk
@@ -19,10 +19,14 @@ class Bee(nyamuk.Nyamuk):
         self.acl_list = None
         self.listener = None
         self.addr = addr
+        
+        self.cm = conn_mgr
+        self.sm = subs_mgr  #subscription manager attached to this bee
     
     def packet_handle(self):
         """Packet Handling Dispatcher."""
         cmd = self.in_packet.command & 0xF0
+        
         if cmd == MV.CMD_CONNECT:
             return self.handle_connect()
         elif cmd == MV.CMD_SUBSCRIBE:
@@ -32,6 +36,7 @@ class Bee(nyamuk.Nyamuk):
             return MV.ERR_NOT_SUPPORTED
     
     def handle_connect(self):
+        """Handle CONNECT command."""
         print "Connecting client = ", self.addr
     
         if self.state != MV.CS_NEW:
@@ -91,7 +96,7 @@ class Bee(nyamuk.Nyamuk):
             print "username Unsupported"
             sys.exit(-1)
         
-        self.id = client_id
+        self.id = client_id.decode()
         self.clean_session = clean_session
         
         if self.will is not None:
@@ -100,12 +105,52 @@ class Bee(nyamuk.Nyamuk):
         
         #ACL
         
+        self.cm.add(self)
+        
         print "New client connected from ", self.addr
         
         return self.send_connack(0)
         
     def handle_subscribe(self):
-        print "Handle subscribe"
+        qos = 0
+        payload = bytearray(0)
+        payloadlen = 0
+        
+        print "Handle subscribe from : ", self.id, " at ", self.addr
+        
+        rc, mid = self.in_packet.read_uint16()
+        if rc != MV.ERR_SUCCESS:
+            return 1
+        
+        while self.in_packet.pos < self.in_packet.remaining_length:
+            rc, ba_sub = self.in_packet.read_string()
+            if rc != MV.ERR_SUCCESS:
+                return 1
+            
+            if len(ba_sub) == 0:
+                print "Empty Subscription from ", self.id, ". Disconnecting.."
+            
+            sub = ba_sub.decode()
+            
+            rc, qos = self.in_packet.read_byte()
+            if rc != MV.ERR_SUCCESS:
+                return 1
+                
+            if qos > 2 or qos < 0:
+                #TODO
+                sys.exit(-1)
+                
+            #fix subtopic TODO
+                
+            rc = self.sm.add(self, sub, qos)
+            
+            if rc == MV.ERR_SUCCESS:
+                sys.exit(-1)
+            
+            payload.append(qos)
+            payloadlen += 1
+        
+        rc = self.send_suback(mid, payloadlen, payload)
         
     def disconnect(self):
         print "[mqtt3_context_disconnect]Unimplemented Func"
@@ -126,4 +171,18 @@ class Bee(nyamuk.Nyamuk):
         pkt.payload[pkt.pos + 1] = result
         
         return self.packet_queue(pkt)
+    
+    def send_suback(self, mid, payloadlen, payload):
+        pkt = MqttPkt()
+        pkt.command = MV.CMD_SUBACK
+        pkt.remaining_length = 2 + payloadlen
         
+        rc = pkt.alloc()
+        if rc != MV.ERR_SUCCESS:
+            return rc
+        
+        pkt.write_uint16(mid)
+        if payloadlen > 0:
+            pkt.write_bytes(payload, payloadlen)
+        
+        return self.packet_queue(pkt)
--- nyamuk/gevmosquittod.py
+++ nyamuk/gevmosquittod.py
@@ -10,13 +10,21 @@ import gevent
 from gevent import monkey; monkey.patch_socket()
 from gevent.server import StreamServer
 
+import subs_mgr
+import conn_mgr
 import bee
 from MV import MV
 
+SubsMgr = subs_mgr.SubscriptionManager()
+ConnMgr = conn_mgr.ConnectionManager()
+
 def handle(sock, addr):
+    global SubsMgr
+    global ConnMgr
+    
     print addr
     
-    b = bee.Bee(sock,addr)
+    b = bee.Bee(sock, addr, ConnMgr, SubsMgr)
 
     rc = b.packet_read(True)
     while rc == MV.ERR_SUCCESS:
