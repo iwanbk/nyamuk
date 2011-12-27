ac936b0e44014409e1faf07c4e4069271a1a67a0
Iwan Budi Kusnanto <iwan.b.kusnanto@gmail.com>
2011-12-27 13:19:49 +0700

    setup.py : OK __init__.py : dunno remove server code

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import Command, setup
+
+config = {
+    'description': 'Nyamuk - Python MQTT Client Library',
+    'author': 'Iwan Budi Kusnanto',
+    'url': '',
+    'download_url': '',
+    'author_email': 'iwan.b.kusnanto@gmail.com',
+    'version': '0.1',
+    'install_requires': [],
+    'packages': ['nyamuk'],
+    'scripts': [],
+    'name': 'Nyamuk'
+}
+
+setup(**config)
