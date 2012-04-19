c9e69294bf33cade431b92edee2600a60d6248c9
Iwan Budi Kusnanto <iwan.b.kusnanto@gmail.com>
2012-04-19 15:52:48 +0700

    upload to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
+import os, sys
 from setuptools import Command, setup
 
-config = {
-    'description': 'Nyamuk - Python MQTT Client Library',
-    'author': 'Iwan Budi Kusnanto',
-    'url': '',
-    'download_url': '',
-    'author_email': 'iwan.b.kusnanto@gmail.com',
-    'version': '0.1',
-    'install_requires': [],
-    'packages': ['nyamuk'],
-    'scripts': [],
-    'name': 'Nyamuk'
-}
+long_description = open('README').read()
 
-setup(**config)
+setup(
+    author = 'Iwan Budi Kusnanto',
+    url = 'https://github.com/iwanbk/nyamuk',
+    author_email ='iwan.b.kusnanto@gmail.com',
+    version ='0.1',
+    install_requires = [],
+    packages = ['nyamuk'],
+    name = 'nyamuk',
+    description = 'Python MQTT Client Library',
+    long_description = long_description,
+    license = "BSD",
+)
