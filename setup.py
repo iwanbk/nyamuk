9e6a82385747c762dc791e1cad3a0b07bfe46d8e
Guillaume Bour <guillaume@bour.cc>
2016-07-02 15:31:43 +0200

    [mgmt] #20: fix setup.py (pypi package builder)

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import os, sys
 from setuptools import Command, setup
 
-long_description = open('README').read()
+long_description = open('README.md').read()
 
 setup(
     author = 'Iwan Budi Kusnanto',
     url = 'https://github.com/iwanbk/nyamuk',
     author_email ='iwan.b.kusnanto@gmail.com',
-    version ='0.1',
+    version ='0.2.0',
     install_requires = [],
     packages = ['nyamuk'],
     name = 'nyamuk',
     description = 'Python MQTT Client Library',
     long_description = long_description,
     license = "BSD",
+    data_files = [
+        ('doc/examples', ['test/pubnya.py', 'test/subnya.py',
+                          'test/simple_publish.py', 'test/simple_subscribe.py']),
+        ('doc', ['README.md'])
+    ],
 )
